72e892ebabd10496fbbd5df0b8bdb02529353209
galortega <galfeort@espol.edu.ec>
2022-05-16 00:41:37 -0500

    feat: implementing layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,12 @@ import { Listbox } from "@headlessui/react";
 import { useState } from "react";
 import Layout from "./components/layout";
 
-import "./styles/App.css";
 
-const people = [
-  { id: 1, name: "Durward Reynolds", unavailable: false },
-  { id: 2, name: "Kenton Towne", unavailable: false },
-  { id: 3, name: "Therese Wunsch", unavailable: false },
-  { id: 4, name: "Benedict Kessler", unavailable: true },
-  { id: 5, name: "Katelyn Rohan", unavailable: false },
-];
 
 function App() {
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
   return (
     <Layout>
-      <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-        <Listbox.Button>{selectedPerson.name}</Listbox.Button>
-        <Listbox.Options>
-          {people.map((person) => (
-            <Listbox.Option
-              key={person.id}
-              value={person}
-              disabled={person.unavailable}
-            >
-              {person.name}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </Listbox>
+      <h1>VEHICLES CRUD</h1>
     </Layout>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <header>
-    <div className="h-screen w-screen flex flex-col ">
+  <Fragment>
+    <header className="w-screen flex flex-col">
       <div className="pt-4 text-2xl mx-auto tracking-widest">
         <a href="/">VEHICLES CRUD</a>
       </div>
+    </header>
+    <div className="h-screen w-screen flex flex-col justify-between align-center items-center">
+      {children}
     </div>
-    <div className="justify-between align-middle items-center">{children}</div>
     <footer className="w-full text-xl text-center py-2 border-t-2 border-black">
       <a href="https://github.com/galortega/roundest-mon">Github</a>
       {" | "}
@@ -15,7 +17,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
       {" | "}
       My Results
     </footer>
-  </header>
+  </Fragment>
 );
 
 export default Layout;
