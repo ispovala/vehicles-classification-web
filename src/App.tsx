94b8344e2b997214495b17c8f3df247159bd791a
galortega <galfeort@espol.edu.ec>
2022-05-17 11:43:29 -0500

    feat: change logo and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Vehicles from "./pages/vehicles";
 import Layout from "./shared/components/layout";
+import logo from "./img/logo.png";
 
 function App() {
   return (
     <Layout>
-      <div className="p-4" />
+      <img className="h-15 w-15 mx-auto my-auto" src={logo} alt="logo"></img>
       <div className="my-auto">
         <Vehicles />
       </div>
