40676c3bcc9a6a4b3969073dd704c0c1bcc732cb
galortega <galfeort@espol.edu.ec>
2022-05-17 11:53:28 -0500

    fix: footer layout on smaller screens

--- src/shared/components/layout.tsx
+++ src/shared/components/layout.tsx
@@ -1,19 +1,19 @@
 import React, { Fragment } from "react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <Fragment>
+  <div className="flex flex-col h-screen justify-between">
     <header className="w-screen flex flex-col border-b-2 border-gray-400">
       <div className="p-4 text-2xl mx-auto tracking-widest">
         <a href="/">VEHICLES CRUD</a>
       </div>
     </header>
-    <div className="h-screen w-screen flex flex-col justify-between align-middle items-center">
+    <div className="w-screen flex flex-col justify-between align-middle items-center">
       {children}
     </div>
-    <footer className="w-full text-xl tracking-wider text-center pt-4 border-t-2 border-gray-400">
+    <footer className="h-4 w-full text-xl tracking-wider text-center pt-4 border-t-2 border-gray-400">
       <a href="https://github.com/galortega/vehicles-client.git">Github</a>
     </footer>
-  </Fragment>
+  </div>
 );
 
 export default Layout;
