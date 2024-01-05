bbe1a153d81ae78dcd0ea17fb48498e103c78e88
Galo O <galfeort@espol.edu.ec>
2024-01-04 19:14:21 -0500

    renaming

--- src/shared/components/layout.tsx
+++ src/shared/components/layout.tsx
@@ -4,7 +4,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <div className="flex flex-col h-screen justify-between">
     <header className="w-screen flex flex-col border-b-2 border-gray-400">
       <div className="p-4 text-2xl mx-auto tracking-widest">
-        <a href="/">VEHICLES CRUD</a>
+        <a href="/">VEHICLES AI</a>
       </div>
     </header>
     <div className="w-screen flex flex-col justify-between align-middle items-center">
