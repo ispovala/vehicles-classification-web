4df449c097f072c354be9d03dc9c134515c9743a
Galo O <galfeort@espol.edu.ec>
2024-01-11 21:13:18 -0500

    Refactor layout component

--- src/shared/components/layout.tsx
+++ src/shared/components/layout.tsx
@@ -1,29 +1,34 @@
-import React, { Fragment } from "react";
+import React from "react";
+import github from "../../img/github.png";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <div className="flex flex-col h-screen justify-between">
+  <div className="relative flex flex-col h-screen justify-between">
     <header className="w-screen flex flex-col border-b-2 border-gray-400">
       <div className="p-4 text-2xl mx-auto tracking-widest">
         <a href="/" className="text-green-900 font-bold">Auto</a>
         <a href="/" className="text-green-800 font-bold">Classif</a>
         <a href="/" className="text-green-600 font-bold text-4xl animate-pulse">AI</a>
       </div>
-      <p className="flex justify-center tracking-wider">Clasificación de vehículos a partir de imágenes usando Redes Neuronales Convolucionales </p>
+      <p className="flex justify-center tracking-wider text-center">Clasificación de vehículos a partir de imágenes usando Redes Neuronales Convolucionales </p>
     </header>
-    <div className="w-screen flex flex-col justify-between align-middle items-center">
+    <div className="w-screen flex flex-col justify-between align-middle items-center my-auto">
       {children}
     </div>
-    <footer className="h-4 w-full text-xl tracking-wider text-center pt-4 border-t-2 border-gray-400 space-y-2">
-      <div className="flex justify-center space-x-4">
-        <p className="tracking-wide font-semibold">•&nbsp;Grupo 8</p>
-        <p className="tracking-wider">•&nbsp;Proyecto de Inteligencia Artificial</p>
-        <a href="https://github.com/ai-grupo8/vehicles-classification-web">•&nbsp;Github</a>
+    <a href="https://github.com/ai-grupo8" className="ml-auto mr-4"><img className="w-10 h-10" src={github} /></a>
+    <div className="p-16" />
+    <footer className="absolute inset-x-0 bottom-0 w-full tracking-wider text-center pt-4 border-t-2 border-gray-400 space-y-2 text-sm md:text- base">
+      <div className="grid grid-rows-2 text-center">
+        <ul className="tracking-wide font-semibold text-base">Grupo 8</ul>
+        <ul className="tracking-wider">Proyecto de Inteligencia Artificial</ul>
       </div>
       <div className="flex justify-center space-x-4">
-        <p>•&nbsp;Johnny Cotrina</p>
-        <p>•&nbsp;Galo Ortega</p>
-        <p>•&nbsp;Isaac Ponce</p>
+        <ul>Johnny Cotrina</ul>
+        <ul>•</ul>
+        <ul>Galo Ortega</ul>
+        <ul>•</ul>
+        <ul>Isaac Ponce</ul>
       </div>
+      <div className="p-1" />
     </footer>
   </div>
 );
